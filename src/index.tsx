32afbc989434868d3006d5bdc020cf5207e56d32
Oren Levi <oren.stu@gmail.com>
2021-02-12 13:54:12 +0200

    Configure hot reload to preserve app state

--- src/index.tsx
+++ src/index.tsx
@@ -15,16 +15,39 @@ configure({
 })
 
 
-const store = new Store({
+let store = new Store({
   json: null
 })
 
+const renderApp = () => {
+  ReactDOM.render(
+    <ThemeProvider>
+      <StoreProvider value={store}>
+        <App />
+      </StoreProvider>
+    </ThemeProvider>,
+    document.getElementById('root')
+  )
+}
 
-ReactDOM.render(
-  <ThemeProvider>
-    <StoreProvider value={store}>
-      <App />
-    </StoreProvider>
-  </ThemeProvider>,
-  document.getElementById('root')
-)
+
+if (module.hot) {
+
+  module.hot.accept([ 'src/features/App' ], () => {
+    // only components code have changed, no need to reload state
+    renderApp()
+  })
+
+  module.hot.accept([ 'src/store' ], () => {
+    // store code have changed
+    const snapshot = store.getSnapshot()
+    console.log('store reload from existing snapshot:', snapshot)
+    store = new Store(snapshot) // try create updated store from the existing snapshot
+    renderApp()
+  })
+
+}
+
+
+renderApp()
+
