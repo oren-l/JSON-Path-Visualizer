c6950a4652e3ab7cb375b00585efbe47cf8a0c00
Oren Levi <oren.stu@gmail.com>
2021-02-14 13:33:59 +0200

    trying out useVirtualList from ahooks

--- src/features/Query/Result/ResultsList.tsx
+++ src/features/Query/Result/ResultsList.tsx
@@ -1,21 +1,29 @@
 import { observer } from 'mobx-react-lite'
 import { useStore } from 'src/store'
 import Paper from '@material-ui/core/Paper'
+import { useVirtualList } from 'ahooks'
 
 export const ResultsList = observer(function ResultsList() {
   const store = useStore()
 
+  const { containerProps, wrapperProps, list } = useVirtualList(store.queryResults, {
+    itemHeight: 60,
+    overscan: 30
+  })
+
   return (
-    <>
-      {
-        store.queryResults.map((result, idx) => (
-          <Paper key={idx} >
-            <pre>
-              {JSON.stringify(result, null, 2)}
-            </pre>
-          </Paper>
-        ))
-      }
-    </>
+    <div {...containerProps}>
+      <div {...wrapperProps}>
+        {
+          list.map(({ data, index }) => (
+            <Paper key={index} >
+              <pre>
+                {JSON.stringify(data, null, 2)}
+              </pre>
+            </Paper>
+          ))
+        }
+      </div>
+    </div>
   )
 })
