977addddfefd86a66c5778320288a6ec9c576cef
Oren Levi <oren.stu@gmail.com>
2021-02-14 12:57:00 +0200

    Add debounce to query input

--- src/features/Query/QueryInput.tsx
+++ src/features/Query/QueryInput.tsx
@@ -1,12 +1,23 @@
 import { observer } from 'mobx-react-lite'
 import { action } from 'mobx'
 import TextField from '@material-ui/core/TextField'
+import { useDebounceEffect } from 'ahooks'
 
 import { useStore } from 'src/store'
+import { useState } from 'react'
 
 
 export const QueryInput = observer(function QueryInput() {
   const store = useStore()
+  const [ temp, setTemp ] = useState<string>(store.query)
+
+  useDebounceEffect(
+    action('query change', () => {
+      store.setQuery(temp)
+    }),
+    [ store, temp ],
+    { wait: 300 }
+  )
 
   return (
     <TextField
@@ -14,8 +25,8 @@ export const QueryInput = observer(function QueryInput() {
       variant='outlined'
       fullWidth
       margin='normal'
-      value={store.query}
-      onChange={action('query change', event => store.setQuery(event.target.value))}
+      value={temp}
+      onChange={event => setTemp(event.target.value)}
     />
   )
 })
