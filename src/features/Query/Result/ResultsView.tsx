ad658028f2ad161955af319babc2cfe612677d7c
Oren Levi <oren.stu@gmail.com>
2021-02-17 17:43:00 +0200

    Refactor ResultsView - extract abort logic

--- src/features/Query/Result/ResultsView.tsx
+++ src/features/Query/Result/ResultsView.tsx
@@ -1,48 +1,19 @@
-import styled from 'styled-components/macro'
 import { observer } from 'mobx-react-lite'
-import Button from '@material-ui/core/Button'
-import CircularProgress from '@material-ui/core/CircularProgress'
 
 import { useStore } from 'src/store'
 import { ResultsList } from './ResultsList'
-
-
-const Spinner = styled(CircularProgress)`
-  margin: ${props => props.theme.spacing(1)}px;
-`
+import { AbortArea } from '../Abort/AbortArea'
 
 
 export const ResultsView = observer(function ResultsView() {
   const store = useStore()
 
-  const showAbortBtn = Boolean(store.loading && store.query)
-  const isAborting = Boolean(store.query?.isAborted)
   const isPartialResults = Boolean(!store.loading && store.query?.isAborted)
   const resultsCount = store.query?.results.length
 
   return (
     <>
-      {
-        showAbortBtn && (
-          <Button
-            variant='contained'
-            fullWidth
-            onClick={store.query?.abort}
-            disabled={store.query?.isAborted}
-          >
-            {
-              isAborting
-                ? (
-                  <>
-                    Aborting...
-                    <Spinner size='1em' />
-                  </>
-                )
-                : 'Abort'
-            }
-          </Button>
-        )
-      }
+      <AbortArea />
       <div>
         <span>
           {`#Results: ${resultsCount}`}
