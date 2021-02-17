cb6bc264bbf9ec8537eeee060090f64fc69aae0f
Oren Levi <oren.stu@gmail.com>
2021-02-17 12:54:54 +0200

    Fix QueryInput not updating when query expression is updated from store

--- src/features/Query/QueryInput.tsx
+++ src/features/Query/QueryInput.tsx
@@ -1,15 +1,26 @@
+import { useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import { action } from 'mobx'
 import TextField from '@material-ui/core/TextField'
 import { useDebounceEffect } from 'ahooks'
 
 import { useStore } from 'src/store'
-import { useState } from 'react'
+import { useAutorun } from 'src/utils/mobx-hooks'
 
 
 export const QueryInput = observer(function QueryInput() {
   const store = useStore()
-  const [ temp, setTemp ] = useState<string>('$')
+  const [ temp, setTemp ] = useState<string>('')
+
+  useAutorun(
+    () => {
+      if (store.query) {
+        setTemp(store.query.expression)
+      }
+    },
+    [ store ],
+    { name: 'update query input from store' },
+  )
 
   useDebounceEffect(
     action('query change', () => {
